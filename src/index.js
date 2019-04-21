78931d464c8aa57f0dd08c6e84826806864365fb
sgoshaa <sgoshaa@yandex.ru>
2019-04-21 16:11:12 +0500

    Добавил функцию ToParseMeteo

--- src/index.js
+++ src/index.js
@@ -4,109 +4,45 @@ import Chart from "chart.js";
 //const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
 //const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
 const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
-//не удалось вернуть массив объектов
-/*const resultatArray=new Array();
-async function loadCurrency() {
-  const response = await fetch(meteoURL);
-  
-  const xmlTest = await response.text();
-  
+
+function toParseMeteo(xmltext,tag) {
+
+  const resultatArray=Object.create(null);
   const parser = new DOMParser();
 
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const forecasts = currencyData.querySelectorAll("FORECAST");
-  const temperatures = currencyData.querySelectorAll("TEMPERATURE");
-  const heat = currencyData.querySelectorAll("HEAT");
-  
-  const resultForecast = Object.create(null);
-  
-  //прогрнозы
- 
+  const meteoData = parser.parseFromString(xmltext, "text/xml");
+  const forecasts = meteoData.querySelectorAll("FORECAST");
+  const weatherData = meteoData.querySelectorAll(tag);
+  //const heat = meteoData.querySelectorAll("HEAT");
   for (let i = 0; i < forecasts.length; i++) {
     const forecastTag = forecasts.item(i);
     const hour = forecastTag.getAttribute("hour");
-    const temperaturaTag = temperatures.item(i);
+    const temperaturaTag = weatherData.item(i);
     const maxTemp = Number.parseInt(temperaturaTag.getAttribute("max"));
     const minTemp = Number.parseInt(temperaturaTag.getAttribute("min"));
     const srtemperatura = (maxTemp+minTemp)/2;
-
-    const heatTag = heat.item(i);
-    const maxHeat = Number.parseInt(heatTag.getAttribute("max"));
-    const minHeat = Number.parseInt(heatTag.getAttribute("min"));
-    const srHeat = (maxHeat+minHeat)/2;
-
-    var resultat = new Object();
-
-    resultForecast[hour] = srtemperatura.toString();
-    resultat.hour = hour;
-    resultat.temperatura = srtemperatura.toString();
-    resultat.heat = srHeat.toString();
-    weakSet.add(resultat);
-
-    resultatArray[i]=resultat;
-
+    resultatArray[hour]=srtemperatura;
   }
-  //result["EUR"] = 1;
-   //result["RANDOM"] = 1 + Math.random();
-  //return resultForecast; //resultatArray;
- return resultatArray;
- // return weakSet;  
-}*/
-/*
- function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
-  }
-  return result;
- }*/
+  return resultatArray;
+ }
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
 buttonBuild.addEventListener("click", async function(){
-//начало получаем и парсим
   const response = await fetch(meteoURL);
   const xmlTest = await response.text();
-  const parser = new DOMParser();
+  const tempData = toParseMeteo(xmlTest,"TEMPERATURE");
+  const heatData = toParseMeteo(xmlTest,"HEAT");
 
-  const meteoData = parser.parseFromString(xmlTest, "text/xml");
-  const forecasts = meteoData.querySelectorAll("FORECAST");
-  const temperatures = meteoData.querySelectorAll("TEMPERATURE");
-  const heat = meteoData.querySelectorAll("HEAT");
- 
-  const arrLabels = new Array();
-  const arrTemp = new Array();
-  const arrHeat = new Array();
-
-  for (let i = 0; i < forecasts.length; i++) {
-    const forecastTag = forecasts.item(i);
-    const hour = forecastTag.getAttribute("hour");
-    const temperaturaTag = temperatures.item(i);
-    const maxTemp = Number.parseInt(temperaturaTag.getAttribute("max"));
-    const minTemp = Number.parseInt(temperaturaTag.getAttribute("min"));
-    const srtemperatura = (maxTemp+minTemp)/2;
-
-    const heatTag = heat.item(i);
-    const maxHeat = Number.parseInt(heatTag.getAttribute("max"));
-    const minHeat = Number.parseInt(heatTag.getAttribute("min"));
-    const srHeat = (maxHeat+minHeat)/2;
-
-    arrLabels[i] = hour;
-    arrTemp[i]   = srtemperatura;
-    arrHeat[i]   = srHeat;
-  }
-    //конец
-  const labelsData = arrLabels;
-  const plotData = arrTemp;
-  const plotData1 = arrHeat;
+  //const plotData = keys.map(key => normalData[key]);
+  const labelsData = Object.keys(tempData);
+  const plotData =  labelsData.map(key =>tempData[key]);
+  const plotData1 = labelsData.map(key =>heatData[key]);
   
   var dataFirst = {
     label: "Температура воздуха по ощущениям,С",
     backgroundColor: "rgb(222, 40, 40)",
     borderColor: "rgb(128, 5, 5)",
-    labelString: "Температура ,С",
     data: plotData1,
   };
      
@@ -170,9 +106,3 @@ buttonBuild.addEventListener("click", async function(){
     window.chart = new Chart(canvasCtx, chartConfig);
   }
 });
-
-/*function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-} */
